4bda352086a0d14c2153b9fe78152c6999aefcff
ragu424 <raguram2817@gmail.com>
2025-01-22 17:34:33 +0530

    Add files via upload

---
+++ Payment Method  Task 05/Testpayment/Model/PaymentMethod.php
@@ -0,0 +1,30 @@
+<?php
+namespace Ragu\Testpayment\Model;
+
+/**
+ * Pay In Store payment method model
+ */
+class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
+{
+    /**
+     * Payment code
+     *
+     * @var string
+     */
+    protected $_code = 'testpayment';
+
+    /**
+     * Authorizes specified amount.
+     *
+     * @param InfoInterface $payment
+     * @param float         $amount
+     *
+     * @return $this
+     *
+     * @throws LocalizedException
+     */
+    public function authorize( \Magento\Payment\Model\InfoInterface $payment, $amount ) 
+    {
+        return $this;
+    }
+}
